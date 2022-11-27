d7e90f609e146772aeab801bd3271f3bc9372889
Jedd <chionglo.jedd@gmail.com>
2022-11-27 03:32:05 -0500

    unable to afford emotes

--- src/regexToText.ts
+++ src/regexToText.ts
@@ -1,130 +1,132 @@
-import { emotes } from './emotes'
+import { emotes } from './emotes';
 
 // Random integer between 0 and max
 function getRandomRange(max: number): number {
-    return Math.floor(Math.random() * max);
+	return Math.floor(Math.random() * max);
 }
 
 // Where is Andy random response
 const verbs = ['Walking', 'Washing', 'Eating'];
 const nouns = ['dog', 'dishes', 'dinner'];
 function whereIsAndy(): string {
-    const verb = verbs[getRandomRange(verbs.length)];
-    const noun = nouns[getRandomRange(nouns.length)];
-    return verb + ' his ' + noun + '.';
+	const verb = verbs[getRandomRange(verbs.length)];
+	const noun = nouns[getRandomRange(nouns.length)];
+	return verb + ' his ' + noun + '.';
 }
 
 const translations = ['Prance forward', 'Shashay left', 'Boogie down', 'Shimmy right'];
 function getTranslation(): string {
-    return translations[getRandomRange(translations.length)];
+	return translations[getRandomRange(translations.length)];
 }
 
 const gamers = ['Rise up!', 'Disperse!', 'Discharge!'];
 function getGamersResponse(): string {
-    return gamers[getRandomRange(gamers.length)];
+	return gamers[getRandomRange(gamers.length)];
 }
 
 function getNextYear(): number {
-    return new Date().getFullYear() + 1;
+	return new Date().getFullYear() + 1;
 }
 
 const regexToText = [
-    {
-        regex: /where.*andy/,
-        getText: () => whereIsAndy()
-    },
-    {
-        regex: /translat(e|ion)/,
-        getText: () => getTranslation()
-    },
-    {
-        regex: /gamers/,
-        getText: () => getGamersResponse()
-    },
-    {
-        regex: /bazinga|zimbabwe/,
-        getText: () => 'Bazinga!'
-    },
-    {
-        regex: /(i'?m|me).*hungr?y/,
-        getText: () => 'Then go eat.'
-    },
-    {
-        regex: /oth(er|a).*side/,
-        getText: () => 'The other what?'
-    },
-    {
-        regex: /take?.*it.*bac?k/,
-        getText: () => 'Now y\'all.'
-    },
-    {
-        regex: /no.*shot/,
-        getText: () => 'Shot.'
-    },
-    {
-        regex: /(?<!no.*)shot/,
-        getText: () => 'No shot.'
-    },
-    {
-        regex: /fa(x|ct(s|ual))/,
-        getText: () => 'No printer.'
-    },
-    {
-        regex: /pam/,
-        getText: () => 'PAM!'
-    },
-    {
-        regex: /when.*andy.*get(ting)?.*new.*computer/,
-        getText: () => getNextYear()
-    },
-    {
-        regex: /too.*late/,
-        getText: () => 'But you promised.'
-    },
-    {
-        regex: /hell\s*halt/,
-        getText: () => `I'm a leak, I'm a leak. ${emotes.sadge ?? ''}`
-    },
-    {
-        regex: /166/,
-        getText: () => 'https://media.discordapp.net/attachments/158049091434184705/795546735594045450/unknown.png'
-    },
-    {
-        regex: /judge?ment/,
-        getText: () => 'https://media.discordapp.net/attachments/837434910486691873/1008836841581072454/judgment.png'
-    },
-    {
-        regex: /shut.*up/,
-        getText: () => 'Smosh voice.'
-    },
-    {
-        regex: /so.*troll?/,
-        getText: () => 'Dyrus.'
-    },
-    {
-        regex: /dyrus/,
-        getText: () => 'So troll.'
-    },
-    {
-        regex: /\boh?\b/,
-        getText: () => 'The misery.'
-    },
-    {
-        regex: /\bno\b(?!.*shot)/,
-        getText: () => 'IDONTTHINKSO.'
-    },
-    {
-        regex: /\bl\b/,
-        getText: () => '+ Ratio.'
-    },
-    {
-        regex: /\b(win|won)\b/,
-        getText: () => 'Boring.'
-    },
-    {
-        regex: /\b(lose|lost)\b/,
-        getText: () => 'Go agane.'
-    }
+	{
+		regex: /where.*andy/,
+		getText: () => whereIsAndy(),
+	},
+	{
+		regex: /translat(e|ion)/,
+		getText: () => getTranslation(),
+	},
+	{
+		regex: /gamers/,
+		getText: () => getGamersResponse(),
+	},
+	{
+		regex: /bazinga|zimbabwe/,
+		getText: () => 'Bazinga!',
+	},
+	{
+		regex: /(i'?m|me).*hungr?y/,
+		getText: () => 'Then go eat.',
+	},
+	{
+		regex: /oth(er|a).*side/,
+		getText: () => 'The other what?',
+	},
+	{
+		regex: /take?.*it.*bac?k/,
+		getText: () => "Now y'all.",
+	},
+	{
+		regex: /no.*shot/,
+		getText: () => 'Shot.',
+	},
+	{
+		regex: /(?<!no.*)shot/,
+		getText: () => 'No shot.',
+	},
+	{
+		regex: /fa(x|ct(s|ual))/,
+		getText: () => 'No printer.',
+	},
+	{
+		regex: /pam/,
+		getText: () => 'PAM!',
+	},
+	{
+		regex: /when.*andy.*get(ting)?.*new.*computer/,
+		getText: () => getNextYear(),
+	},
+	{
+		regex: /too.*late/,
+		getText: () => 'But you promised.',
+	},
+	{
+		regex: /hell\s*halt/,
+		getText: () => `I'm a leak, I'm a leak. ${emotes.sadge ?? ''}`,
+	},
+	{
+		regex: /166/,
+		getText: () =>
+			'https://media.discordapp.net/attachments/158049091434184705/795546735594045450/unknown.png',
+	},
+	{
+		regex: /judge?ment/,
+		getText: () =>
+			'https://media.discordapp.net/attachments/837434910486691873/1008836841581072454/judgment.png',
+	},
+	// {
+	//     regex: /shut.*up/,
+	//     getText: () => 'Smosh voice.'
+	// },
+	{
+		regex: /so.*troll?/,
+		getText: () => 'Dyrus.',
+	},
+	{
+		regex: /dyrus/,
+		getText: () => 'So troll.',
+	},
+	{
+		regex: /\boh?\b/,
+		getText: () => 'The misery.',
+	},
+	{
+		regex: /\bno\b(?!.*shot)/,
+		getText: () => 'IDONTTHINKSO.',
+	},
+	{
+		regex: /\bl\b/,
+		getText: () => '+ Ratio.',
+	},
+	{
+		regex: /\b(win|won)\b/,
+		getText: () => 'Boring.',
+	},
+	{
+		regex: /\b(lose|lost)\b/,
+		getText: () => 'Go agane.',
+	},
 ];
 
 export default regexToText;
