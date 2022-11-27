cf5ad77e2c8c8fea72f81ef8a8b627a4720e976e
Jedd <chionglo.jedd@gmail.com>
2022-11-27 01:28:20 -0500

    making fat guy short

--- src/regexToAudio.ts
+++ src/regexToAudio.ts
@@ -1,68 +1,68 @@
 const regexToAudio = [
-    {
-        regex: /w(oah|hoa)/,
-        audio: 'basementgang'
-    },
-    {
-        regex: /(thunder vs lightning)/,
-        audio: 'thunder_vs_lightning_full'
-    },
-    {
-        regex: /demon/,
-        audio: 'demontime'
-    },
-    {
-        regex: /i'?m.*(4|four|poor|bored)/,
-        audio: 'VillagerCWhat3'
-    },
-    {
-        regex: /((yo)?u|yu).*(no|know)|sigh/,
-        audio: 'sykkuno'
-    },
-    {
-        regex: /uh.*oh/,
-        audio: 'uhohstinky'
-    },
-    {
-        regex: /(tbc.*hype|focus.*up)/,
-        audio: 'tbchype'
-    },
-    {
-        regex: /suc(c|k|tion)/,
-        audio: 'suction'
-    },
-    {
-        regex: /stop/,
-        audio: 'itstimetostop'
-    },
-    {
-        regex: /dog/,
-        audio: 'whatthedogdoin'
-    },
-    {
-        regex: /bean/,
-        audio: 'beans'
-    },
-    {
-        regex: /smosh|shut.*up/,
-        audio: 'smosh_shut_up'
-    },
-    {
-        regex: /\bno\b(?!.*shot)|noidontthinkso/,
-        audio: 'NOIDONTTHINKSO'
-    },
-    {
-        regex: /fulcrum|c(o|u)me?/,
-        audio: 'fulcrum_come_in'
-    },
-    {
-        regex: /\bl\b/,
-        audio: 'obliterated'
-    },
-    {
-        regex: /good morning|morning|gm|donda/,
-        audio: 'good_morning_donda'
-    }
+	{
+		regex: /w(oah|hoa)/,
+		audio: 'basementgang',
+	},
+	{
+		regex: /(thunder vs lightning)/,
+		audio: 'thunder_vs_lightning_full',
+	},
+	{
+		regex: /demon/,
+		audio: 'demontime',
+	},
+	{
+		regex: /i'?m.*(4|four|poor|bored)/,
+		audio: 'VillagerCWhat3',
+	},
+	{
+		regex: /((yo)?u|yu).*(no|know)|sigh/,
+		audio: 'sykkuno',
+	},
+	{
+		regex: /uh.*oh/,
+		audio: 'uhohstinky',
+	},
+	{
+		regex: /(tbc.*hype|focus.*up)/,
+		audio: 'tbchype',
+	},
+	{
+		regex: /suc(c|k|tion)/,
+		audio: 'suction',
+	},
+	{
+		regex: /stop/,
+		audio: 'itstimetostop',
+	},
+	{
+		regex: /dog/,
+		audio: 'whatthedogdoin',
+	},
+	{
+		regex: /bean/,
+		audio: 'beans',
+	},
+	{
+		regex: /smosh|shut.*up/,
+		audio: 'smosh_shut_up',
+	},
+	{
+		regex: /\bno\b(?!.*shot)|noidontthinkso/,
+		audio: 'NOIDONTTHINKSO',
+	},
+	{
+		regex: /fulcrum|c(o|u)me?/,
+		audio: 'fulcrum_come_in',
+	},
+	{
+		regex: /\bl\b/,
+		audio: 'obliterated',
+	},
+	{
+		regex: /good morning|morning|gm|donda/,
+		audio: 'good_morning_donda',
+	},
 ];
 
 export default regexToAudio;
