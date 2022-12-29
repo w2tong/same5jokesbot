74ee9c926e3e83aafb65df313f804a9ddfbfac89
Jedd <chionglo.jedd@gmail.com>
2022-12-28 23:08:07 -0500

    ruhroh

--- src/regexToText.ts
+++ src/regexToText.ts
@@ -127,6 +127,14 @@ const regexToText = [
 		regex: /\b(lose|lost)\b/,
 		getText: () => 'Go agane.',
 	},
+	{
+		regex: /boring/,
+		getText: () => "Don't care.",
+	},
+	{
+		regex: /don'?t care/,
+		getText: () => 'Boring.',
+	},
 ];
 
 export default regexToText;
