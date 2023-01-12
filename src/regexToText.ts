da59f9cd101310cbffa1954c2567cdcd48459002
Wayne Tong <dkotrzarashi@gmail.com>
2023-01-12 17:34:40 -0500

    Add with rizz text joke

--- src/regexToText.ts
+++ src/regexToText.ts
@@ -132,7 +132,11 @@ const regexToText = [
     {
         regex: /don'?t care/,
         getText: () => 'Boring.'
-    }
+    },
+    {
+        regex: /please|pl(s|z)/,
+        getText: () => '(with rizz)'
+    },
 ];
 
 export default regexToText;
