481625d9ced79329fc31d1e40406020d88b7f184
Wayne Tong <dkotrzarashi@gmail.com>
2023-03-10 06:52:27 -0500

    Move interactionCreate handler to interactionCreate.ts

---
+++ src/events/interactionCreate.ts
@@ -0,0 +1,28 @@
+import { GuildMember, Interaction } from "discord.js";
+import { createPlayer, createVoiceConnection, playAudioFile } from "../voice";
+
+export default async (interaction: Interaction): Promise<void> => {
+    if (!interaction.isChatInputCommand()) return;
+    const { commandName } = interaction;
+    // Play audio
+    if (commandName === 'play' && interaction.member instanceof GuildMember && interaction.guild && interaction.member.voice.channel) {
+        const voiceConnection = {
+            channelId: interaction.member.voice.channel.id,
+            guildId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator,
+            selfDeaf: false
+        }
+        const audioFile = interaction.options.getString('audio') ?? ''
+        const reply = interaction.member.voice ? `Playing ${audioFile}.` : 'You are not in a voice channel.';
+        interaction.reply({ content: reply, ephemeral: true });
+        const player = createPlayer();
+        const connection = createVoiceConnection(voiceConnection, player, interaction.client);
+        await playAudioFile(connection, player, audioFile, interaction.member.user.username)
+    }
+    // Reply with a number between 1 and 100 (or min and max)
+    else if (commandName === 'roll') {
+        const min = interaction.options.getInteger('min') ?? 1;
+        const max = interaction.options.getInteger('max') ?? 100;
+        interaction.reply(Math.floor(Math.random() * (max + 1 - min) + min).toString());
+    }
+}
