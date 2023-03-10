ae530f1d548e9aa1f3883fb3c68c4c80aa89e000
Wayne Tong <dkotrzarashi@gmail.com>
2023-03-10 06:46:07 -0500

    Move messageCreate handler to messageCreate.ts

--- src/index.ts
+++ src/index.ts
@@ -2,63 +2,15 @@ import { Client, GatewayIntentBits, Message, TextChannel, Interaction, GuildMemb
 import * as dotenv from 'dotenv';
 dotenv.config();
 import createCronJobs from './create-cronjobs';
-import regexToText from './regex-to-text';
-import regexToAudio from './regex-to-audio';
-import regexToReact from './regex-to-react';
 import { getEmotes } from './emotes';
 import { getMomentCurrentTimeEST } from './util'
 import { createPlayer, createVoiceConnection, playAudioFile } from './voice'
+import messageCreateHandler from './events/messageCreate';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildVoiceStates] });
 let mainChannel: TextChannel | undefined;
 
-// Responses to text messages
-client.on(Events.MessageCreate, async (message: Message): Promise<void> => {
-    // Don't respond to bots
-    if (message.author.bot) return;
-    // Don't respond to Bot Abuser role
-    if (message.member && message.member.roles.cache.some(role => role.name === 'Bot Abuser')) return;
-    // Return if incorrect channel type
-    if (message.channel.type !== ChannelType.GuildText) return;
-
-    const command = message.content.toLowerCase();
-
-    //React with emoji
-    for (const regexReact of regexToReact) {
-        const react = regexReact.getReact()
-        if (react && regexReact.regex.test(command)) {
-            message.react(react);
-        }
-    }
-    // Text replies
-    let botMessage = '';
-    for (let regexText of regexToText) {
-        if (regexText.regex.test(command) && message.member) {
-            const text = regexText.getText(command, message.member?.displayName);
-            botMessage += `${text}\n`;
-        }
-    }
-    // Send message
-    if (botMessage) {
-        message.channel.send(botMessage).catch((e) => console.log(`Error sending message: ${e}`));;
-    }
-    // Audio replies
-    for (let regexAudio of regexToAudio) {
-        const audio = regexAudio.getAudio();
-        if (regexAudio.regex.test(command) && audio && message.member && message.member.voice.channel && message.guild) {
-            const voiceConnection = {
-                channelId: message.member.voice.channel.id,
-                guildId: message.guild.id,
-                adapterCreator: message.guild.voiceAdapterCreator,
-                selfDeaf: false
-            }
-            const player = createPlayer();
-            const connection = createVoiceConnection(voiceConnection, player);
-            await playAudioFile(connection, player, audio, message.member.user.username);
-            break;
-        }
-    }
-});
+client.on(Events.MessageCreate, messageCreateHandler);
 
 // Slash commands
 client.on(Events.InteractionCreate, async (interaction: Interaction): Promise<void> => {
@@ -152,4 +104,4 @@ client.once(Events.ClientReady, (): void => {
     console.log('Same5JokesBot online.');
 });
 
-export default client
+export default client;
