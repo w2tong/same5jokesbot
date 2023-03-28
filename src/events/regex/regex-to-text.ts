821984f1e36c6a9bdea2fb20f4a2f326efa0872e
Wayne Tong <dkotrzarashi@gmail.com>
2023-03-27 20:35:58 -0400

    Fix gamers Disperse odds

--- src/events/regex/regex-to-text.ts
+++ src/events/regex/regex-to-text.ts
@@ -16,7 +16,7 @@ function getTranslation(): string {
     return translations[getRandomRange(translations.length)];
 }
 
-const gamers = ['Rise up!', 'Disperse!', 'Discharge!', 'Disperse!'];
+const gamers = ['Rise up!', 'Disperse!', 'Discharge!'];
 function getGamersResponse(): string {
     return gamers[getRandomRange(gamers.length)];
 }
