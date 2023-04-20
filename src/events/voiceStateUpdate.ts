6c8a4d085014941d2b77448f539f0630e9df3d25
Wayne Tong <dkotrzarashi@gmail.com>
2023-04-20 05:41:09 -0400

    Update conditions for bot to disconnect

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,7 +1,7 @@
 import { ChannelType, VoiceState } from 'discord.js';
 import { logError } from '../logger';
 import { getMomentCurrentTimeEST } from '../util';
-import { disconnectVoice, joinVoice, playAudioFile } from '../voice';
+import { disconnectVoice, isInGuildVoice, joinVoice, playAudioFile } from '../voice';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,9 +10,24 @@ export default (oldState: VoiceState, newState: VoiceState) => {
     // Stop if user is a bot
     if (oldState.member?.user.bot) return;
 
-    // Disconnect if users in current bot channel disconnect or move to AFK channel
-    if ((newState.channelId === null || newState.guild.afkChannelId === newState.channelId) && (oldState.channel?.members.size === 0 || (oldState.channel?.members.size === 1 && process.env.CLIENT_ID && oldState.channel?.members.has(process.env.CLIENT_ID)))) {
-        disconnectVoice(oldState.guild.id);
+    // Disconnect bot when users leave voice channel
+    if (isInGuildVoice(oldState.guild.id)) {
+        // Disconnect if all users in current bot channel disconnect or move to AFK channel
+        if (
+            (newState.channelId === null || newState.guild.afkChannelId === newState.channelId) && 
+            (oldState.channel?.members.size === 1 && process.env.CLIENT_ID && oldState.channel?.members.has(process.env.CLIENT_ID))
+        ) {
+            disconnectVoice(oldState.guild.id);
+        }
+
+        // Disconnect if no users in any non-AFK voice channels
+        let count = 0;
+        oldState.guild.channels.cache.forEach((channel) => {
+            if (channel.type === ChannelType.GuildVoice && channel.id !== channel.guild.afkChannelId) count += channel.members.size;
+        });
+        if (count === 1) {
+            disconnectVoice(oldState.guild.id);
+        }
     }
 
     // Stop if user moves to AFK channel
@@ -26,9 +41,6 @@ export default (oldState: VoiceState, newState: VoiceState) => {
             mainChannel.send('You made Azi leave.').catch(logError);
         }
     }
-
-    // Stop if voice state update is itself
-    if (oldState.id === newState.client.user?.id) return;
 
     // Play teleporting fat guy when moving between channels
     if (oldState.channelId && newState.channelId && oldState.channelId != newState.channelId) {
