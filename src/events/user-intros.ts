d52900461efeed19dcce1de29c35f5ff2931dba8
Wayne Tong <dkotrzarashi@gmail.com>
2023-04-24 20:16:38 -0400

    Add Jed intro

--- src/events/user-intros.ts
+++ src/events/user-intros.ts
@@ -3,8 +3,7 @@ dotenv.config();
 
 const userIntros: {[key: string]: string} = {};
 
-if (process.env.ALDA_ID) {
-    userIntros[process.env.ALDA_ID] = 'all_the';
-}
+if (process.env.ALDA_ID) userIntros[process.env.ALDA_ID] = 'all_the';
+if (process.env.JED_ID) userIntros[process.env.JED_ID] = 'arthur_knitter_short';
 
 export default userIntros;
