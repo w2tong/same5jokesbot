06be0c5902af2411b8c9742d64ee0643b6f90b66
Wayne Tong <dkotrzarashi@gmail.com>
2023-05-05 05:36:27 -0400

    Add pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 module.exports = {
     apps : [{
         name: 'same5jokesbot',
@@ -6,5 +9,15 @@ module.exports = {
         ignore_watch: ['node_modules','src/audio','logs','.env'],
         log_file: 'logs/pm2.log',
         log_date_format: 'YYYY-MM-DD HH:mm:ss'
-    }]
+    }],
+    deploy: {
+        production: {
+            host: process.env.HOST,
+            user: process.env.USER,
+            ref: 'origin/main', // (use 'origin/master' for your master branch,
+            repo: 'git@github.com:w2tong/Same5JokesBot.git', // your repo url
+            path: '/home/opc/Same5JokesBot',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production && pm2 save'
+        }
+    }
 };
