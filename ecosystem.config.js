362241df3efd2f36f8dacb11d9650bcaff4cb5d3
Wayne Tong <dkotrzarashi@gmail.com>
2023-05-06 05:41:34 -0400

    Separate pm2 out and error logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,8 @@ module.exports = {
         script: 'npm start',
         watch: true,
         ignore_watch: ['node_modules','src/audio','logs','.env'],
-        log_file: 'logs/pm2.log',
+        out_file: 'pm2.out.log',
+        error_file: 'pm2.error.log',
         log_date_format: 'YYYY-MM-DD HH:mm:ss'
     }],
     deploy: {
