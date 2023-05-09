0c4f3543b56c5d28abd7cce0aaec342c25861c97
Wayne Tong <dkotrzarashi@gmail.com>
2023-05-08 20:53:12 -0400

    Update reminders to use chrono-node to parse dates

--- src/commands/remind.ts
+++ src/commands/remind.ts
@@ -1,6 +1,6 @@
-import { ChannelType, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { ChannelType, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder,  } from 'discord.js';
+import * as chrono from 'chrono-node';
 import { newReminder } from '../reminders';
-import { parseDate } from '../util';
 import { getUserRemindersCount } from '../sql/reminders';
 import { convertDateToUnixTimestamp } from '../util';
 
@@ -10,8 +10,7 @@ async function execute(interaction: ChatInputCommandInteraction) {
     await interaction.deferReply();
     const mention = interaction.options.getMentionable('mention');
     const msg = interaction.options.getString('message');
-    const time = interaction.options.getNumber('time');
-    const timeUnit = interaction.options.getString('time-unit');
+    const time = interaction.options.getString('time');
     const message = `${mention} ${msg} (sent by ${interaction.user.username})`;
     
     const count = await getUserRemindersCount(interaction.user.id);
@@ -19,21 +18,30 @@ async function execute(interaction: ChatInputCommandInteraction) {
     if (count >= MAX_REMINDERS) {
         reply += `\nMax number of reminders is ${MAX_REMINDERS}.`;
     }
-    else if (interaction.channel && interaction.channel.type === ChannelType.GuildText && time && timeUnit) {
-        const date = parseDate(time, timeUnit);
-        const unixTimestamp = convertDateToUnixTimestamp(date);
-        if (await newReminder(interaction.user.id ,interaction.channel, date, message) === true) {
-            const embed = new EmbedBuilder()
-                .setTitle('Reminder')
-                .addFields(
-                    { name: 'Time', value: `<t:${unixTimestamp}> <t:${unixTimestamp}:R>` },
-                    { name: 'Message', value: `${message}` }
-                );
-            void interaction.editReply({embeds: [embed]});
-            return;
+    else if (interaction.channel && interaction.channel.type === ChannelType.GuildText && time) {
+        const dateStr = chrono.parseDate(time, {timezone: 'ET'});
+        if (dateStr) {
+            const date = new Date(dateStr);
+            const unixTimestamp = convertDateToUnixTimestamp(date);
+            if (await newReminder(interaction.user.id ,interaction.channel, date, message) === true) {
+                const embed = new EmbedBuilder()
+                    .setTitle('Reminder')
+                    .addFields(
+                        { name: 'Time', value: `<t:${unixTimestamp}> <t:${unixTimestamp}:R>` },
+                        { name: 'Message', value: `${message}` }
+                    );
+                void interaction.editReply({embeds: [embed]});
+                return;
+            }
         }
+        else {
+            void interaction.editReply(`Invalid time/date input: \`${time}\``);
+        }
+        
     }
-    void interaction.editReply(reply);
+    else {
+        void interaction.editReply(reply);
+    }
 }
 
 const name = 'remind';
@@ -42,12 +50,7 @@ const commandBuilder = new SlashCommandBuilder()
     .setName(name)
     .setDescription('Set a reminder to mention someone with a message.')
     .addMentionableOption((option) => option.setName('mention').setDescription('Select user or role.').setRequired(true))
-    .addNumberOption((option) => option.setName('time').setDescription('Amount of time. Must be greater than 0.').setRequired(true).setMinValue(0.1))
-    .addStringOption((option) => option.setName('time-unit').setDescription('Time unit (eg. seconds, minutes, hours).').setRequired(true).addChoices(
-        {name: 'minutes', value: 'minute'},
-        {name: 'hours', value: 'hour'},
-        {name: 'days', value: 'day'}
-    ))
+    .addStringOption((option) => option.setName('time').setDescription('Enter a time or date (e.g. "in 5 minutes" or "friday at 5pm")').setRequired(true))
     .addStringOption((option) => option.setName('message').setDescription('Message').setRequired(true));
 
 export default { execute, name, commandBuilder };
