61366c85c5bb6da05f4ed11823074f1133a8f354
Wayne Tong <dkotrzarashi@gmail.com>
2023-06-08 18:18:08 -0400

    Update interactionCreate syntax

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -3,16 +3,13 @@ import { commands } from '../commands/commands';
 import { logError } from '../logger';
 
 export default (interaction: Interaction) => {
-    if (interaction.isChatInputCommand()) {
-        const { commandName } = interaction;
-        if (commands[commandName]) {
-            try {
-                commands[commandName](interaction);
-            }
-            catch (err) {
-                logError(err);
-                interaction.reply('Error executing command.').catch(logError);
-            }
+    if (interaction.isChatInputCommand() && commands[interaction.commandName]) {
+        try {
+            commands[interaction.commandName](interaction);
+        }
+        catch (err) {
+            logError(err);
+            interaction.reply('Error executing command.').catch(logError);
         }
     }
     else {
