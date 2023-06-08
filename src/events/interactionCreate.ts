ef08fb4a9c9d84b889469fa4609a55a9df1dde6d
Wayne Tong <dkotrzarashi@gmail.com>
2023-06-08 16:53:04 -0400

    Refactor commands.ts

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -5,16 +5,13 @@ import { logError } from '../logger';
 export default (interaction: Interaction) => {
     if (interaction.isChatInputCommand()) {
         const { commandName } = interaction;
-        for (const command of commands) {
-            if (command.name === commandName) {
-                try {
-                    command.execute(interaction);
-                    return;
-                }
-                catch (err) {
-                    logError(err);
-                    interaction.reply('Error executing command.').catch(logError);
-                }
+        if (commands[commandName]) {
+            try {
+                commands[commandName](interaction);
+            }
+            catch (err) {
+                logError(err);
+                interaction.reply('Error executing command.').catch(logError);
             }
         }
     }
