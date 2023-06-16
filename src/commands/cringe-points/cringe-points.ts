c322856d7f7d9e51eb4df1ce3de5052a7bb956fa
Wayne Tong <dkotrzarashi@gmail.com>
2023-06-16 00:28:40 -0400

    Add give cringe points command

--- src/commands/cringe-points/cringe-points.ts
+++ src/commands/cringe-points/cringe-points.ts
@@ -2,11 +2,13 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import getCringePoints from './subcommands/get-cringe-points';
 import getTopCringePoints from './subcommands/get-top-cringe-points';
 import gambleCringePoints from './subcommands/gamble-cringe-points';
+import giveCringePoints from './subcommands/give-cringe-points';
 
 const subcommands = {
     [getCringePoints.name]: getCringePoints.execute,
     [getTopCringePoints.name]: getTopCringePoints.execute,
-    [gambleCringePoints.name]: gambleCringePoints.execute
+    [gambleCringePoints.name]: gambleCringePoints.execute,
+    [giveCringePoints.name]: giveCringePoints.execute
 };
 
 async function execute(interaction: ChatInputCommandInteraction) {
@@ -21,6 +23,7 @@ const commandBuilder = new SlashCommandBuilder()
     .setDescription('Get or gamble Cringe points.')
     .addSubcommand(getCringePoints.subcommandBuilder)
     .addSubcommand(getTopCringePoints.subcommandBuilder)
-    .addSubcommand(gambleCringePoints.subcommandBuilder);
+    .addSubcommand(gambleCringePoints.subcommandBuilder)
+    .addSubcommand(giveCringePoints.subcommandBuilder);
 
 export default { execute, name, commandBuilder };
