7727d6df8572b8278c7249f293fd1e0d9b61d27f
Wayne Tong <dkotrzarashi@gmail.com>
2023-06-16 04:05:47 -0400

    Update user intros with audioFileMap

--- src/events/user-intros.ts
+++ src/events/user-intros.ts
@@ -1,3 +1,5 @@
+import audio from '../audioFileMap';
+
 const ALDA_ID = '147095136709509120';
 const ANDYD_ID = '213516916705198080';
 const AZI_ID = '180881117547593728';
@@ -8,12 +10,12 @@ const MICHAEL_ID = '164856525863518209';
 const WAYNE_ID = '158048359591051274';
 
 const userIds: {[key:string]: string} = {
-    [ALDA_ID]: 'all_the',
-    [ANDYD_ID]: 'wolf_howl',
-    [CALVIN_ID]: 'blacken_chinese_man',
-    [JED_ID]: 'what_a_fd_up_day',
-    [MICHAEL_ID]: 'this_is_library',
-    [WAYNE_ID]: 'doc_chinese_motorcycle'
+    [ALDA_ID]: audio.allThe,
+    [ANDYD_ID]: audio.wolfHowl,
+    [CALVIN_ID]: audio.blackenChineseMan,
+    [JED_ID]: audio.whatAFdUpDay,
+    [MICHAEL_ID]: audio.thisIsLibrary,
+    [WAYNE_ID]: audio.docChineseMotorcycle
 };
 
 export default userIds;
