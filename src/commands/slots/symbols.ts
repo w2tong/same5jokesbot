0c16a0255672674c9a31fe6f5e4762f122e6aa5e
Wayne Tong <dkotrzarashi@gmail.com>
2023-06-20 04:55:33 -0400

    Update slots symbols

--- src/commands/slots/symbols.ts
+++ src/commands/slots/symbols.ts
@@ -12,8 +12,8 @@ const slotsSymbols: {[key: string]: SlotsSymbol} = {
     peepoYell:      {pc: 0.45, mult: 4.5, emote: Emotes.peepoYell, backupEmote: ''},
     OkaygeBusiness: {pc: 0.5, mult: 5, emote: Emotes.OkaygeBusiness, backupEmote: ''},
     vacation:       {pc: 0.6, mult: 6, emote: Emotes.vacation, backupEmote: ''},
-    borpaSpin:      {pc: 0.8, mult: 10, emote: Emotes.borpaSpin, backupEmote: ''},
-    ChugU:          {pc: 1, mult: 15, emote: Emotes.ChugU, backupEmote: ''}
+    borpaSpin:      {pc: 0.8, mult: 8, emote: Emotes.borpaSpin, backupEmote: ''},
+    ChugU:          {pc: 1, mult: 10, emote: Emotes.ChugU, backupEmote: ''}
 };
 
 function repeatSymbolArray (symbol: SlotsSymbol, repeat: number) {
@@ -23,12 +23,12 @@ function repeatSymbolArray (symbol: SlotsSymbol, repeat: number) {
 type SlotsSymbol = {pc: number, mult: number, emote: string, backupEmote: string};
 const symbols = [
     ...repeatSymbolArray(slotsSymbols.OMEGALAUGHING, 10),
-    ...repeatSymbolArray(slotsSymbols.NOOO, 9),
-    ...repeatSymbolArray(slotsSymbols.DESKCHAN, 8),
-    ...repeatSymbolArray(slotsSymbols.Clueless, 7),
-    ...repeatSymbolArray(slotsSymbols.pawgchamp, 6),
-    ...repeatSymbolArray(slotsSymbols.gachiBASS, 5),
-    ...repeatSymbolArray(slotsSymbols.THIS, 5),
+    ...repeatSymbolArray(slotsSymbols.NOOO, 10),
+    ...repeatSymbolArray(slotsSymbols.DESKCHAN, 10),
+    ...repeatSymbolArray(slotsSymbols.Clueless, 9),
+    ...repeatSymbolArray(slotsSymbols.pawgchamp, 8),
+    ...repeatSymbolArray(slotsSymbols.gachiBASS, 7),
+    ...repeatSymbolArray(slotsSymbols.THIS, 6),
     ...repeatSymbolArray(slotsSymbols.peepoYell, 5),
     ...repeatSymbolArray(slotsSymbols.OkaygeBusiness, 4),
     ...repeatSymbolArray(slotsSymbols.vacation, 3),
@@ -51,7 +51,7 @@ const specialSymbols: {[key: number]: Array<SpecialSymbol>} = {
     25: [
         {mult: 10, freeSpins: 1, emote: '', backupEmote: '1️⃣'}
     ],
-    50 : [
+    50: [
         {mult: 10, freeSpins: 0, emote: '', backupEmote: '💰'},
     ],
     100: [
