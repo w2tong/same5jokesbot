2e16dcd20320cb6b3124e363daa7da3de6fb4752
Wayne Tong <dkotrzarashi@gmail.com>
2023-07-10 04:10:57 -0400

    Add option to buy max amount of lottery tickets

--- src/commands/lottery/subcommands/buy/auto.ts
+++ src/commands/lottery/subcommands/buy/auto.ts
@@ -3,17 +3,33 @@ import lottery from '../../lotteryManager';
 import lotteryManager from '../../lotteryManager';
 
 async function execute(interaction: ChatInputCommandInteraction) {
-    const user = interaction.user;
     await interaction.deferReply({ephemeral: true});
-    const {success, res} = await lotteryManager.buyTicket(user.id, lottery.generateNumbersArray());
-    void interaction.editReply(res);
-    if (success) void interaction.channel?.send(`${user} bought a lottery ticket.`);
+    const user = interaction.user;
+    const msgs = [];
+    const buyMax = interaction.options.getBoolean('buy-max');
+
+    const ticketsToBuy = buyMax ? lottery.choose : 1;
+    let ticketsBought = 0;
+
+    while (ticketsBought < ticketsToBuy) {
+        const {success, res} = await lotteryManager.buyTicket(user.id, lottery.generateNumbersArray());
+        msgs.push(res);
+        if (!success) break;
+        ticketsBought++;
+    }
+    
+    void interaction.editReply(msgs.join('\n'));
+    if (ticketsBought > 0) void interaction.channel?.send(`${user} bought ${ticketsBought} lottery ticket${ticketsBought > 1  ? 's' : ''}.`);
 }
 
 const name = 'auto';
 
 const subcommandBuilder = new SlashCommandSubcommandBuilder()
     .setName(name)
-    .setDescription(`Buy a lottery ticket (${lottery.price} points). Numbers are automatically picked. Max of ${lottery.ticketLimit} tickets.`);
+    .setDescription(`Buy a lottery ticket (${lottery.price} points). Numbers are automatically picked. Max of ${lottery.ticketLimit} tickets.`)
+    .addBooleanOption(option => option
+        .setName('buy-max')
+        .setDescription('Buy the max amount of tickets')
+    );
 
 export default { execute, name, subcommandBuilder };
