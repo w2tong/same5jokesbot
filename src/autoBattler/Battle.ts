f89fdaa0f6b64d95c9de66cc57d268c4b7f1de7e
Wayne Tong <dkotrzarashi@gmail.com>
2023-09-11 21:10:17 -0400

    add auto battler Battle and Character classes, util funcs

---
+++ src/autoBattler/Battle.ts
@@ -0,0 +1,112 @@
+import { EmbedBuilder } from 'discord.js';
+import Character from './Character';
+import { mapCharNameHealth, rollDice } from './util';
+import { emptyEmbedFieldInline } from '../util/discordUtil';
+
+enum Side {
+    Left = 'left',
+    Right = 'right'
+}
+
+class Battle {
+    private left: Character[];
+    private leftAlive: Set<number>;
+
+    private right: Character[];
+    private rightAlive: Set<number> = new Set();
+
+    private charTurn = 0;
+    private turnOrder: {char: Character, init: number, side: Side}[] = [];
+    private combatHistory: string[] = [];
+
+    constructor(left: Character[], right: Character[]) {
+        this.left = left;
+        this.leftAlive = new Set(Array(left.length).keys());
+
+        this.right = right;
+        this.rightAlive = new Set(Array(right.length).keys());
+    }
+
+    startCombat() {
+        // Assign turn order for characters
+        for (const char of this.left) {
+            const init = rollDice(1, 20) + char.initiativeBonus;
+            this.turnOrder.push({char, init, side: Side.Left});
+        }
+        for (const char of this.right) {
+            const init = rollDice(1, 20) + char.initiativeBonus;
+            this.turnOrder.push({char, init, side: Side.Right});
+        }
+        this.turnOrder.sort((a, b) => b.init - a.init);
+    }
+
+    nextTurn(): {combatEnded: boolean, winner?: Side} {
+        if (this.leftAlive.size === 0) {
+            return {combatEnded: true, winner: Side.Right};
+            // right wins
+        }
+        else if (this.rightAlive.size === 0) {
+            return {combatEnded: true, winner: Side.Left};
+        }
+        else {
+            const char = this.turnOrder[this.charTurn++].char;
+            const side = this.turnOrder[this.charTurn++].side;
+
+            // Set target if current character has no target
+            if (char.target === null) {
+                let targets: Character[];
+                if (side === Side.Left) {
+                    targets = Array.from(this.rightAlive.values()).map(i => this.right[i]);
+                }
+                else {
+                    targets = Array.from(this.leftAlive.values()).map(i => this.left[i]);
+                }
+                char.setRandomTarget(targets);
+            }
+
+            // Attempt to attack target
+            if (char.target !== null) {
+                const result = char.attackTarget();
+                if (result) {
+                    if (result.hit) {
+                        this.combatHistory.push(`${char.name} attacked ${char.target.name} (${result.attackDetails}) for ${result.damage ?? 0} damage (${result.damageDetails}).`);
+                    }
+                    else {
+                        this.combatHistory.push(`${char.name} attacked ${char.target.name} and missed (${result.attackDetails}).`);
+                    }
+                    // Remove target from alive characters and set target to null
+                    if (char.target.isDead()) {
+                        if (side === Side.Left) {
+                            this.rightAlive.delete(char.target.index);
+                        }
+                        else {
+                            this.leftAlive.delete(char.target.index);
+                        }
+                        char.target = null;
+                    }
+                }
+            }
+            return {combatEnded: false};
+        }
+    }
+
+    generateEmbed(): EmbedBuilder {
+        const leftNames = this.left.map(mapCharNameHealth);
+        const rightNames = this.right.map(mapCharNameHealth);
+
+        const embed = new EmbedBuilder()
+            .setTitle('auto battle')
+            .addFields(
+                {name: 'Combatants', value: leftNames.join('\n')},
+                emptyEmbedFieldInline,
+                {name: 'Combatants', value: rightNames.join('\n')},
+
+                {name: 'Combat History', value: this.combatHistory.join('\n')}
+            )
+        ;
+
+        return embed;
+    }
+}
+
+export default Battle;
