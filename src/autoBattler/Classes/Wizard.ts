d49e230d5391ca88ff3ca43b6a37c8c6952997eb
Wayne Tong <dkotrzarashi@gmail.com>
2023-09-16 04:09:48 -0400

    remove done TODO comment

--- src/autoBattler/Classes/Wizard.ts
+++ src/autoBattler/Classes/Wizard.ts
@@ -13,7 +13,6 @@ class Wizard extends Character {
         if (this.target) {
             this.currMana = 0;
             this.battle.combatLog.add(`${bold(this.name)} casted ${bold('Firebolt')}.`);
-            // TODO: change this attack to firebolt
             const attack = this.attackRoll();
             if (attack.hitType === HitType.Hit || attack.hitType === HitType.Crit) {
                 let damage = rollDice(this.damage);
