98cba76750f896828ef5ad477f36fd6dff34223a
Wayne Tong <dkotrzarashi@gmail.com>
2023-10-03 17:00:42 -0400

    [auto-battler] separate player and npc stats, add goblin and orc npcs

--- src/autoBattler/encounters.ts
+++ src/autoBattler/encounters.ts
@@ -5,7 +5,7 @@ import Rogue from './Classes/Rogue';
 import Wizard from './Classes/Wizard';
 import { Equipment, defaultEquipment } from './Equipment/Equipment';
 import { weapons } from './Equipment/Weapons';
-import { CharacterStatTemplate, ClassStats, RatStats } from './statTemplates';
+import { CharacterStatTemplate, NPCStats } from './statTemplates';
 
 function createCharsFunc(chars: {class: typeof Character, stats: CharacterStatTemplate, equipment: Equipment, name: string}[], level: number, count: number) {
     return () => {
@@ -30,22 +30,33 @@ function createCharsFunc(chars: {class: typeof Character, stats: CharacterStatTe
 type EncounterParams = {level: number, count: number}
 function fighterEncounter({level, count}: EncounterParams) {
     return createCharsFunc([
-        {class: Fighter, stats: ClassStats.Fighter, equipment: defaultEquipment.Fighter, name: 'Fighter'}
+        {class: Fighter, stats: NPCStats.Fighter, equipment: defaultEquipment.Fighter, name: NPCStats.Fighter.className}
     ], level, count);
 }
 function rogueEncounter({level, count}: EncounterParams) {
     return createCharsFunc([
-        {class: Rogue, stats: ClassStats.Rogue, equipment: defaultEquipment.Rogue, name: 'Rogue'}
+        {class: Rogue, stats: NPCStats.Rogue, equipment: defaultEquipment.Rogue, name: NPCStats.Rogue.className}
     ], level, count);
 }
 function wizardEncounter({level, count}: EncounterParams) {
     return createCharsFunc([
-        {class: Wizard, stats: ClassStats.Wizard, equipment: defaultEquipment.Wizard, name: 'Wizard'}
+        {class: Wizard, stats: NPCStats.Wizard, equipment: defaultEquipment.Wizard, name: NPCStats.Wizard.className}
     ], level, count);
 }
 function ratEncounter({level, count}: EncounterParams) {
     return createCharsFunc([
-        {class: Character, stats: RatStats, equipment: {mainHand: weapons.poisonbite0}, name: 'Rat'}
+        {class: Character, stats: NPCStats.Rat, equipment: {mainHand: weapons.poisonbite0}, name: NPCStats.Rat.className}
+    ], level, count);
+}
+function goblinEncounter({level, count}: EncounterParams) {
+    return createCharsFunc([
+        {class: Fighter, stats: NPCStats.GoblinFighter, equipment: defaultEquipment.Fighter, name: NPCStats.GoblinFighter.className},
+        {class: Rogue, stats: NPCStats.GoblinRogue, equipment: defaultEquipment.Rogue, name: NPCStats.GoblinRogue.className}
+    ], level, count);
+}
+function orcEncounter({level, count}: EncounterParams) {
+    return createCharsFunc([
+        {class: Fighter, stats: NPCStats.OrcFighter, equipment: defaultEquipment.Fighter, name: NPCStats.OrcFighter.className},
     ], level, count);
 }
 
@@ -54,31 +65,40 @@ const encounters: {[key: number]: Array<() => Character[]>} = {
         fighterEncounter({level: 1, count: 1}),
         rogueEncounter({level: 1, count: 1}),
         wizardEncounter({level: 1, count: 1}),
-        ratEncounter({level: 1, count: 3})
+        ratEncounter({level: 1, count: 3}),
+        orcEncounter({level: 1, count: 1}),
     ],
     2: [
         fighterEncounter({level: 2, count: 1}),
         rogueEncounter({level: 2, count: 1}),
         wizardEncounter({level: 2, count: 1}),
-        ratEncounter({level: 2, count: 3})
+        ratEncounter({level: 2, count: 3}),
+        goblinEncounter({level: 1, count: 1}),
+        orcEncounter({level: 2, count: 1}),
     ],
     3: [
         fighterEncounter({level: 3, count: 1}),
         rogueEncounter({level: 3, count: 1}),
         wizardEncounter({level: 3, count: 1}),
-        ratEncounter({level: 3, count: 3})
+        ratEncounter({level: 3, count: 3}),
+        goblinEncounter({level: 2, count: 1}),
+        orcEncounter({level: 3, count: 1}),
     ],
     4: [
         fighterEncounter({level: 4, count: 1}),
         rogueEncounter({level: 4, count: 1}),
         wizardEncounter({level: 4, count: 1}),
-        ratEncounter({level: 4, count: 3})
+        ratEncounter({level: 4, count: 4}),
+        goblinEncounter({level: 3, count: 1}),
+        orcEncounter({level: 4, count: 1}),
     ],
     5: [
         fighterEncounter({level: 5, count: 1}),
         rogueEncounter({level: 5, count: 1}),
         wizardEncounter({level: 5, count: 1}),
-        ratEncounter({level: 5, count: 3})
+        ratEncounter({level: 5, count: 4}),
+        goblinEncounter({level: 4, count: 1}),
+        orcEncounter({level: 5, count: 1}),
     ],
 };
 
--- src/autoBattler/statTemplates.ts
+++ src/autoBattler/statTemplates.ts
@@ -49,12 +49,12 @@ type CharacterStatTemplate = {
     };
 }
 
-const ClassStats: {[name in ClassName]: CharacterStatTemplate} = {
+const PlayerStats: {[name in ClassName]: CharacterStatTemplate} = {
     [ClassName.Fighter]: {
         className: 'Fighter',
         attackBonus: {
             base: 0,
-            perLvl: 0.5
+            perLvl: 0.25
         },
         damageBonus: {
             base: 0,
@@ -62,7 +62,7 @@ const ClassStats: {[name in ClassName]: CharacterStatTemplate} = {
         },
         armourClass: {
             base: 10,
-            perLvl: 0.5
+            perLvl: 0.25
         },
         physDR: {
             base: 0,
@@ -102,7 +102,7 @@ const ClassStats: {[name in ClassName]: CharacterStatTemplate} = {
         className: 'Rogue',
         attackBonus: {
             base: 0,
-            perLvl: 0.5
+            perLvl: 0.25
         },
         damageBonus: {
             base: 0,
@@ -110,7 +110,7 @@ const ClassStats: {[name in ClassName]: CharacterStatTemplate} = {
         },
         armourClass: {
             base: 10,
-            perLvl: 0.5
+            perLvl: 0.25
         },
         physDR: {
             base: 0,
@@ -150,7 +150,7 @@ const ClassStats: {[name in ClassName]: CharacterStatTemplate} = {
         className: 'Wizard',
         attackBonus: {
             base: 0,
-            perLvl: 0.5
+            perLvl: 0.25
         },
         damageBonus: {
             base: 0,
@@ -158,7 +158,7 @@ const ClassStats: {[name in ClassName]: CharacterStatTemplate} = {
         },
         armourClass: {
             base: 10,
-            perLvl: 0.5
+            perLvl: 0.25
         },
         physDR: {
             base: 0,
@@ -196,54 +196,350 @@ const ClassStats: {[name in ClassName]: CharacterStatTemplate} = {
     }
 };
 
-// Rat
-const RatStats: CharacterStatTemplate = {
-    className: 'Rat',
-    attackBonus: {
-        base: -2,
-        perLvl: 0.5
+const NPCStats: {[name in ClassName|'Rat'|'GoblinFighter'|'GoblinRogue'|'OrcFighter']: CharacterStatTemplate} = {
+    // Classes
+    [ClassName.Fighter]: {
+        className: 'Fighter',
+        attackBonus: {
+            base: 1,
+            perLvl: 0.5
+        },
+        damageBonus: {
+            base: 0,
+            perLvl: 1
+        },
+        armourClass: {
+            base: 11,
+            perLvl: 0.5
+        },
+        physDR: {
+            base: 0,
+            perLvl: 0
+        },
+        magicDR: {
+            base: 0,
+            perLvl: 0
+        },
+        physResist: {
+            base: 5,
+            perLvl: 1
+        },
+        magicResist: {
+            base: 5,
+            perLvl: 1
+        },
+        health: {
+            base: 20,
+            perLvl: 8
+        },
+        mana: 10, 
+        manaPerAtk: {
+            base: 0,
+            perLvl: 0.1
+        },
+        manaRegen: {
+            base: 1,
+            perLvl: 0.1
+        },
+        initiativeBonus: {
+            base: 0,
+            perLvl: 1
+        }
     },
-    damageBonus: {
-        base: 0,
-        perLvl: 0.5
+    [ClassName.Rogue]: {
+        className: 'Rogue',
+        attackBonus: {
+            base: 1,
+            perLvl: 0.5
+        },
+        damageBonus: {
+            base: 0,
+            perLvl: 1
+        },
+        armourClass: {
+            base: 11,
+            perLvl: 0.5
+        },
+        physDR: {
+            base: 0,
+            perLvl: 0
+        },
+        magicDR: {
+            base: 0,
+            perLvl: 0
+        },
+        physResist: {
+            base: 0,
+            perLvl: 0.5
+        },
+        magicResist: {
+            base: 0,
+            perLvl: 0.5
+        },
+        health: {
+            base: 20,
+            perLvl: 6
+        },
+        mana: 10, 
+        manaPerAtk: {
+            base: 0,
+            perLvl: 0.1
+        },
+        manaRegen: {
+            base: 1,
+            perLvl: 0.1
+        },
+        initiativeBonus: {
+            base: 0,
+            perLvl: 1
+        }
     },
-    armourClass: {
-        base: 8,
-        perLvl: 0.5
+    [ClassName.Wizard]: {
+        className: 'Wizard',
+        attackBonus: {
+            base: 1,
+            perLvl: 0.5
+        },
+        damageBonus: {
+            base: 0,
+            perLvl: 1
+        },
+        armourClass: {
+            base: 11,
+            perLvl: 0.5
+        },
+        physDR: {
+            base: 0,
+            perLvl: 0
+        },
+        magicDR: {
+            base: 0,
+            perLvl: 0
+        },
+        physResist: {
+            base: 0,
+            perLvl: 0
+        },
+        magicResist: {
+            base: 5,
+            perLvl: 1
+        },
+        health: {
+            base: 20,
+            perLvl: 5
+        },
+        mana: 10, 
+        manaPerAtk: {
+            base: 0,
+            perLvl: 0.1
+        },
+        manaRegen: {
+            base: 2,
+            perLvl: 0.1
+        },
+        initiativeBonus: {
+            base: 0,
+            perLvl: 1
+        }
     },
-    physDR: {
-        base: 0,
-        perLvl: 0
-    },
-    magicDR: {
-        base: 0,
-        perLvl: 0
-    },
-    physResist: {
-        base: 0,
-        perLvl: 0.25
-    },
-    magicResist: {
-        base: 0,
-        perLvl: 0.25
-    },
-    health: {
-        base: 3,
-        perLvl: 1
-    },
-    mana: 0, 
-    manaPerAtk: {
-        base: 0,
-        perLvl: 0
-    },
-    manaRegen: {
-        base: 0,
-        perLvl: 0
-    },
-    initiativeBonus: {
-        base: 0,
-        perLvl: 1
-    }
-};
 
-export { CharacterStatTemplate, ClassStats, RatStats };
+    // Rat
+    Rat: {
+        className: 'Rat',
+        attackBonus: {
+            base: -2,
+            perLvl: 0.5
+        },
+        damageBonus: {
+            base: 0,
+            perLvl: 0.5
+        },
+        armourClass: {
+            base: 8,
+            perLvl: 0.5
+        },
+        physDR: {
+            base: 0,
+            perLvl: 0
+        },
+        magicDR: {
+            base: 0,
+            perLvl: 0
+        },
+        physResist: {
+            base: 0,
+            perLvl: 0
+        },
+        magicResist: {
+            base: 0,
+            perLvl: 0
+        },
+        health: {
+            base: 4,
+            perLvl: 2
+        },
+        mana: 0, 
+        manaPerAtk: {
+            base: 0,
+            perLvl: 0
+        },
+        manaRegen: {
+            base: 0,
+            perLvl: 0
+        },
+        initiativeBonus: {
+            base: 2,
+            perLvl: 1
+        }
+    },
+
+    // Goblins
+    GoblinFighter: {
+        className: 'Goblin Fighter',
+        attackBonus: {
+            base: 0,
+            perLvl: 0.5
+        },
+        damageBonus: {
+            base: 0,
+            perLvl: 0.67
+        },
+        armourClass: {
+            base: 12,
+            perLvl: 0.5
+        },
+        physDR: {
+            base: 0,
+            perLvl: 0
+        },
+        magicDR: {
+            base: 0,
+            perLvl: 0
+        },
+        physResist: {
+            base: 10,
+            perLvl: 1
+        },
+        magicResist: {
+            base: 0,
+            perLvl: 0
+        },
+        health: {
+            base: 10,
+            perLvl: 5
+        },
+        mana: 0, 
+        manaPerAtk: {
+            base: 0,
+            perLvl: 0
+        },
+        manaRegen: {
+            base: 0,
+            perLvl: 0
+        },
+        initiativeBonus: {
+            base: 0,
+            perLvl: 1
+        }
+    },
+    GoblinRogue: {
+        className: 'Goblin Rogue',
+        attackBonus: {
+            base: 0,
+            perLvl: 0.5
+        },
+        damageBonus: {
+            base: 0,
+            perLvl: 0.67
+        },
+        armourClass: {
+            base: 10,
+            perLvl: 0.5
+        },
+        physDR: {
+            base: 0,
+            perLvl: 0
+        },
+        magicDR: {
+            base: 0,
+            perLvl: 0
+        },
+        physResist: {
+            base: 5,
+            perLvl: 1
+        },
+        magicResist: {
+            base: 0,
+            perLvl: 0
+        },
+        health: {
+            base: 10,
+            perLvl: 4
+        },
+        mana: 0, 
+        manaPerAtk: {
+            base: 0,
+            perLvl: 0
+        },
+        manaRegen: {
+            base: 0,
+            perLvl: 0
+        },
+        initiativeBonus: {
+            base: 2,
+            perLvl: 1
+        }
+    },
+
+    // Orcs
+    OrcFighter: {
+        className: 'Orc Fighter',
+        attackBonus: {
+            base: 2,
+            perLvl: 0.5
+        },
+        damageBonus: {
+            base: 1,
+            perLvl: 1
+        },
+        armourClass: {
+            base: 12,
+            perLvl: 0.5
+        },
+        physDR: {
+            base: 1,
+            perLvl: 0
+        },
+        magicDR: {
+            base: 1,
+            perLvl: 0
+        },
+        physResist: {
+            base: 10,
+            perLvl: 1
+        },
+        magicResist: {
+            base: 10,
+            perLvl: 1
+        },
+        health: {
+            base: 25,
+            perLvl: 10
+        },
+        mana: 10, 
+        manaPerAtk: {
+            base: 0,
+            perLvl: 0.1
+        },
+        manaRegen: {
+            base: 1,
+            perLvl: 0.1
+        },
+        initiativeBonus: {
+            base: -2,
+            perLvl: 1
+        }
+    }
+} as const;
+
+export { CharacterStatTemplate, PlayerStats, NPCStats };
