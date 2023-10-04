f61a7a22d452a67848944b983bc8b6e3c4c40912
Wayne Tong <dkotrzarashi@gmail.com>
2023-10-04 14:45:56 -0400

    [auto-battler] rename armour items

--- src/autoBattler/Equipment/Armour.ts
+++ src/autoBattler/Equipment/Armour.ts
@@ -83,7 +83,7 @@ const armour: {[id in ArmourId]: Armour} = {
     lightarmour0: {
         id: 'lightarmour0',
         itemType: ItemType.Armour,
-        name: 'Light Armour',
+        name: 'Leather Armour',
         type: ArmourType.Light,
         armourClass: 1,
         manaRegen: 5
@@ -91,7 +91,7 @@ const armour: {[id in ArmourId]: Armour} = {
     lightarmour1: {
         id: 'lightarmour1',
         itemType: ItemType.Armour,
-        name: 'Light Armour +1',
+        name: 'Leather Armour +1',
         type: ArmourType.Light,
         armourClass: 2,
         manaRegen: 6
@@ -99,7 +99,7 @@ const armour: {[id in ArmourId]: Armour} = {
     lightarmour2: {
         id: 'lightarmour2',
         itemType: ItemType.Armour,
-        name: 'Light Armour +2',
+        name: 'Leather Armour +2',
         type: ArmourType.Light,
         armourClass: 3,
         manaRegen: 7
@@ -107,7 +107,7 @@ const armour: {[id in ArmourId]: Armour} = {
     lightarmour3: {
         id: 'lightarmour3',
         itemType: ItemType.Armour,
-        name: 'Light Armour +3',
+        name: 'Leather Armour +3',
         type: ArmourType.Light,
         armourClass: 4,
         manaRegen: 8
@@ -115,7 +115,7 @@ const armour: {[id in ArmourId]: Armour} = {
     lightarmour4: {
         id: 'lightarmour4',
         itemType: ItemType.Armour,
-        name: 'Light Armour +4',
+        name: 'Leather Armour +4',
         type: ArmourType.Light,
         armourClass: 5,
         manaRegen: 9
@@ -123,7 +123,7 @@ const armour: {[id in ArmourId]: Armour} = {
     lightarmour5: {
         id: 'lightarmour5',
         itemType: ItemType.Armour,
-        name: 'Light Armour +5',
+        name: 'Leather Armour +5',
         type: ArmourType.Light,
         armourClass: 6,
         manaRegen: 10
@@ -131,7 +131,7 @@ const armour: {[id in ArmourId]: Armour} = {
     mediumarmour0: {
         id: 'mediumarmour0',
         itemType: ItemType.Armour,
-        name: 'Medium Armour',
+        name: 'Chainmail',
         type: ArmourType.Medium,
         armourClass: 2,
         manaRegen: 1
@@ -139,7 +139,7 @@ const armour: {[id in ArmourId]: Armour} = {
     mediumarmour1: {
         id: 'mediumarmour1',
         itemType: ItemType.Armour,
-        name: 'Medium Armour +1',
+        name: 'Chainmail +1',
         type: ArmourType.Medium,
         armourClass: 3,
         manaRegen: 2
@@ -147,7 +147,7 @@ const armour: {[id in ArmourId]: Armour} = {
     mediumarmour2: {
         id: 'mediumarmour2',
         itemType: ItemType.Armour,
-        name: 'Medium Armour +2',
+        name: 'Chainmail +2',
         type: ArmourType.Medium,
         armourClass: 4,
         manaRegen: 3
@@ -155,7 +155,7 @@ const armour: {[id in ArmourId]: Armour} = {
     mediumarmour3: {
         id: 'mediumarmour3',
         itemType: ItemType.Armour,
-        name: 'Medium Armour +3',
+        name: 'Chainmail +3',
         type: ArmourType.Medium,
         armourClass: 5,
         manaRegen: 4
@@ -163,7 +163,7 @@ const armour: {[id in ArmourId]: Armour} = {
     mediumarmour4: {
         id: 'mediumarmour4',
         itemType: ItemType.Armour,
-        name: 'Medium Armour +4',
+        name: 'Chainmail +4',
         type: ArmourType.Medium,
         armourClass: 6,
         manaRegen: 5
@@ -171,7 +171,7 @@ const armour: {[id in ArmourId]: Armour} = {
     mediumarmour5: {
         id: 'mediumarmour5',
         itemType: ItemType.Armour,
-        name: 'Medium Armour +5',
+        name: 'Chainmail +5',
         type: ArmourType.Medium,
         armourClass: 2,
         manaRegen: 6
@@ -179,14 +179,14 @@ const armour: {[id in ArmourId]: Armour} = {
     heavyarmour0: {
         id: 'heavyarmour0',
         itemType: ItemType.Armour,
-        name: 'Heavy Armour',
+        name: 'Plate Armour',
         type: ArmourType.Heavy,
         armourClass: 3,
     },
     heavyarmour1: {
         id: 'heavyarmour1',
         itemType: ItemType.Armour,
-        name: 'Heavy Armour +1',
+        name: 'Plate Armour +1',
         type: ArmourType.Heavy,
         armourClass: 4,
         physDR: 1,
@@ -194,7 +194,7 @@ const armour: {[id in ArmourId]: Armour} = {
     heavyarmour2: {
         id: 'heavyarmour2',
         itemType: ItemType.Armour,
-        name: 'Heavy Armour +2',
+        name: 'Plate Armour +2',
         type: ArmourType.Heavy,
         armourClass: 5,
         physDR: 2,
@@ -202,7 +202,7 @@ const armour: {[id in ArmourId]: Armour} = {
     heavyarmour3: {
         id: 'heavyarmour3',
         itemType: ItemType.Armour,
-        name: 'Heavy Armour +3',
+        name: 'Plate Armour +3',
         type: ArmourType.Heavy,
         armourClass: 6,
         physDR: 3,
@@ -210,7 +210,7 @@ const armour: {[id in ArmourId]: Armour} = {
     heavyarmour4: {
         id: 'heavyarmour4',
         itemType: ItemType.Armour,
-        name: 'Heavy Armour +4',
+        name: 'Plate Armour +4',
         type: ArmourType.Heavy,
         armourClass: 7,
         physDR: 4,
@@ -218,7 +218,7 @@ const armour: {[id in ArmourId]: Armour} = {
     heavyarmour5: {
         id: 'heavyarmour5',
         itemType: ItemType.Armour,
-        name: 'Heavy Armour +5',
+        name: 'Plate Armour +5',
         type: ArmourType.Heavy,
         armourClass: 8,
         physDR: 5,
