92aa7742f03068c99bd396cc943a975afdae4e53
Wayne Tong <dkotrzarashi@gmail.com>
2023-10-06 04:23:25 -0400

    [auto-battler] add belts to loot table

--- src/autoBattler/Equipment/Belt.ts
+++ src/autoBattler/Equipment/Belt.ts
@@ -10,15 +10,73 @@ interface Belt extends Item {
 
 // TODO: add belts
 type BeltId = 
-'belt'
+'chargesBelt0' | 'chargesBelt1'
+| 'effBelt0' | 'effBelt1' | 'effBelt2' | 'effBelt3'
+| 'healBelt0' | 'healBelt1' | 'healBelt2' | 'healBelt3'
 ;
 
 const belts: {[id in BeltId]: Belt} = {
-    'belt': {
+    chargesBelt0: {
+        id: 'chargesBelt0',
+        itemType: ItemType.Belt,
+        name: 'Belt of Charges',
+        charges: 1
+    },
+    chargesBelt1: {
         id: 'belt',
         itemType: ItemType.Belt,
-        name: 'Belt',
-    }
+        name: 'Belt of Greater Charges',
+        charges: 2
+    },
+    effBelt0: {
+        id: 'effBelt0',
+        itemType: ItemType.Belt,
+        name: 'Belt of Effectiveness',
+        effectiveness: 25
+    },
+    effBelt1: {
+        id: 'effBelt1',
+        itemType: ItemType.Belt,
+        name: 'Belt of Greater Effectiveness',
+        effectiveness: 50
+    },
+    effBelt2: {
+        id: 'effBelt2',
+        itemType: ItemType.Belt,
+        name: 'Belt of Superior Effectiveness',
+        effectiveness: 75
+    },
+    effBelt3: {
+        id: 'effBelt3',
+        itemType: ItemType.Belt,
+        name: 'Belt of Supreme Effectiveness',
+        effectiveness: 100
+    },
+    healBelt0: {
+        id: 'healBelt0',
+        itemType: ItemType.Belt,
+        name: 'Belt of Healing',
+        healing: 5
+    },
+    healBelt1: {
+        id: 'healBelt1',
+        itemType: ItemType.Belt,
+        name: 'Belt of Greater Healing',
+        healing: 10
+    },
+    healBelt2: {
+        id: 'healBelt2',
+        itemType: ItemType.Belt,
+        name: 'Belt of Superior Healing',
+        healing: 20
+    },
+    healBelt3: {
+        id: 'healBelt3',
+        itemType: ItemType.Belt,
+        name: 'Belt of Supreme Healing',
+        healing: 40
+    },
+
 } as const;
 
 function getBeltTooltip(belt: Belt) {
